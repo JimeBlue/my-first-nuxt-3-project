d3abc96a9ab738cf9d27eb69c6ea48f2331afd32
Jimena Miguez <j.miguez@airlst.com>
2023-10-17 10:07:48 +0200

    Have input value to be emitted by FormKit

--- app.vue
+++ app.vue
@@ -1,24 +1,15 @@
-<!-- NOTE: PricingPlan -->
-<!-- IMPORTANT: 
-- the label comes from FormKit 
-- help text comes form Formkit
-
-- this is typical vue 3
-:options="radioOptions"
-:modelValue="selectedOption"
-@input="updateSelectedOption"
--->
 <template>
   <div>
-    <FormKit
-      :type="pp"
-      label="Select Your Pricing Plan"
-      :options="radioOptions"
-      help="What is the best plan for you?"
-      :modelValue="selectedOption"
-      @input="updateSelectedOption"
-    />
-    Selected Value (Parent Component): {{ selectedOption }}
+    <FormKit type="form" v-model="values">
+      <FormKit
+        :type="pp"
+        label="Select Your Pricing Plan"
+        :options="radioOptions"
+        help="What is the best plan for you?"
+      />
+
+      <pre wrap>{{ values }}</pre>
+    </FormKit>
   </div>
 </template>
 <script setup>
@@ -26,10 +17,10 @@ import { ref } from 'vue';
 import { createInput } from '@formkit/vue';
 import PricingPlan from '~/formkit-components/PricingPlan.vue';
 
+const values = ref({});
 const pp = createInput(PricingPlan, {
   props: ['options'],
 });
-const selectedOption = ref(null);
 
 const radioOptions = ref([
   {
@@ -60,32 +51,4 @@ const radioOptions = ref([
     img: 'rocket.png',
   },
 ]);
-
-const updateSelectedOption = (value) => {
-  console.log('Received value in parent: ', value);
-  selectedOption.value = value;
-};
 </script>
-
-<!-- NOTE: RadioGroup -->
-<!-- <template>
-  <div>
-    <h1>Radio Group Example</h1>
-    <RadioGroup
-      :modelValue="selectedValue"
-      :options="options"
-      @update:modelValue="selectedValue = $event"
-    />
-
-    <p>Selected Value (Parent Component): {{ selectedValue }}</p>
-  </div>
-</template> -->
-
-<!-- <script setup>
-import { ref } from 'vue';
-import RadioGroup from '~/formkit-components/RadioGroup.vue';
-
-const selectedValue = ref(null);
-
-const options = ['Option A', 'Option B', 'Option C', 'Option D']; // Define your options here
-</script> -->
