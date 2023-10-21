08c07d16b7319520bcc0ae036da218e0bcdc89de
Jimena Miguez <j.miguez@airlst.com>
2023-10-21 18:26:38 +0200

    Add check icon to checked radio input

--- formkit-components/PricingPlan.vue
+++ formkit-components/PricingPlan.vue
@@ -11,14 +11,31 @@
     >
       <div class="flex justify-between">
         <span class="text-sm text-slate-700">{{ option.label }}</span>
-        <input
-          type="radio"
-          :id="'option' + index"
-          :value="option.value"
-          @input="handleInput($event)"
-          :checked="props.context._value === option.value"
-          :name="'option' + index"
-        />
+        <div class="relative">
+          <input
+            type="radio"
+            :id="'option' + index"
+            :value="option.value"
+            @input="handleInput($event)"
+            :checked="props.context._value === option.value"
+            :name="'option' + index"
+            class="hidden"
+          />
+
+          <svg
+            v-if="props.context._value === option.value"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-check-circle-fill w-4 h-4 text-blue-500 absolute top-0 right-0"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"
+            />
+          </svg>
+        </div>
       </div>
       <div class="w-10 h-10">
         <img
